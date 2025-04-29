736e7ea5c2cd41ccef02d60e3c80029bf9bd09a6
ginal001 <ginal123456789@gmail.com>
2025-04-29 16:45:42 +0200

    added parsing in main

--- main.py
+++ main.py
@@ -7,4 +7,32 @@ def morse():
 def cesar():
     pass
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Narzędzie do szyfrowania/odszyfrowania plików: Cezar lub Morse'
+    )
 
+    # tryb: encrypt vs decrypt
+    mode_group = parser.add_mutually_exclusive_group(required=True)
+    mode_group.add_argument('-e', '--encrypt', action='store_true',
+                            help='uruchom tryb szyfrowania')
+    mode_group.add_argument('-d', '--decrypt', action='store_true',
+                            help='uruchom tryb odszyfrowywania')
+
+    # algorytm: Cezar vs Morse
+    cipher_group = parser.add_mutually_exclusive_group(required=True)
+    cipher_group.add_argument('-c', '--caesar', action='store_true',
+                              help='użyj szyfru Cezara')
+    cipher_group.add_argument('-m', '--morse', action='store_true',
+                              help='użyj kodu Morse’a')
+
+    # opcja przesunięcia dla Cezara
+    parser.add_argument('-n', type=int, default=3,
+                        help='(Cezar) wartość przesunięcia liter (domyślnie 3)')
+
+    # pliki wejściowy i wyjściowy
+    parser.add_argument('input_file', help='ścieżka do pliku wejściowego')
+    parser.add_argument('output_file', help='ścieżka do pliku wyjściowego')
+
+    args = parser.parse_args()
+    print(args)
